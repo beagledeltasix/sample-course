7f0b08ee2186468fefac5592e82aa67f13f5166f
beagledeltasix <mikesk813@gmail.com>
2022-02-16 15:47:15 -0800

    sample docker course frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello there! Working? maybe! Let's see!@@@
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          no way
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
